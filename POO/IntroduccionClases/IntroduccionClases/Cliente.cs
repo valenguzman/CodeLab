d0fdb9058e95ee2f3cce827eafc25fba88dd8124
valenguzman <a2510754@institutmontilivi.cat>
2026-01-16 18:55:05 +0100

    Ex POO Cliente

---
+++ POO/IntroduccionClases/IntroduccionClases/Cliente.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntroduccionClases
+{
+    public class Cliente
+    {
+        public string nombre;
+        public string email;
+        public string direccion;
+        public string telefono;
+        public int edad;
+
+        public Cliente()
+        {
+            nombre = "Por defecto";
+        }
+
+        public Cliente(string nombre, string email, string direccion, string telefono, int edad)
+        {
+            this.nombre = nombre;
+            this.email = email;
+            this.direccion = direccion;
+            this.telefono = telefono;
+            this.edad = edad;
+
+        }
+
+    }
+}
---
+++ POO/IntroduccionClases/IntroduccionClases/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace IntroduccionClases
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente cliente = new Cliente();
+
+            Console.WriteLine(cliente.nombre);
+            Console.WriteLine(cliente.email);
+            Console.WriteLine(cliente.direccion);
+            Console.WriteLine(cliente.telefono);
+            Console.WriteLine(cliente.edad);
+        }
+    }
+}
